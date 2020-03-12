d9d2741da663bd581e0f25321eb6caebf74f622d
Newt <itsnewtt@outlook.com>
2020-03-12 07:18:19 +0000

    Complete lecture 3 - Display Hard Coded Questions and Answers

---
+++ Lecture 3. Display Hard Coded Questions and Answers/game.js
@@ -0,0 +1,98 @@
+// fetch elements
+const question = document.querySelector('#question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
+// prepare variables
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript??",
+    choice1: "<script>",
+    choice2: "<javascript>",
+    choice3: "<js>",
+    choice4: "<scripting>",
+    answer: 1
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choice1: "<script href='xxx.js'>",
+    choice2: "<script name='xxx.js'>",
+    choice3: "<script src='xxx.js'>",
+    choice4: "<script file='xxx.js'>",
+    answer: 3
+  },
+  {
+    question: " How do you write 'Hello World' in an alert box?",
+    choice1: "msgBox('Hello World');",
+    choice2: "alertBox('Hello World');",
+    choice3: "msg('Hello World');",
+    choice4: "alert('Hello World');",
+    answer: 4
+  }
+];
+
+// constants
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+// startGame() - resets all variables, primes the game
+const startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+
+  getNewQuestion();
+}
+
+// getNewQuestion() - fetches a new question
+const getNewQuestion = () => {
+  // check if any questions are left
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    // go to the end page
+    return window.location.assign('/end.html')
+  }
+
+  // increment the question counter by 1
+  questionCounter++;
+
+  // calculate the question's index and fetch the current question
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+
+  // display the current question
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice => {
+    const number = choice.dataset['number'];
+    choice.innerText = currentQuestion[`choice${number}`];
+  });
+
+  // remove the used question and begin accepting answers
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+// adds listeners to each choice
+choices.forEach(choice => {
+  choice.addEventListener('click', e => {
+    // if answers are not being accepted, ignore all input
+    if (!acceptingAnswers) return;
+
+    // stop accepting answers, and fetch the selected choice
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset['number'];
+
+    // load a new question
+    getNewQuestion();
+  })
+});
+
+
+startGame();
--- readme.md
+++ readme.md
@@ -5,7 +5,7 @@
 
 1. [Create and Style the Home Page](https://github.com/itsnewt/Build-A-Quiz-App-With-HTML-CSS-and-JavaScript/tree/master/Lecture%201.%20Create%20and%20Style%20the%20Home%20Page)
 2. [Create and Style the Game Page](https://github.com/itsnewt/Build-A-Quiz-App-With-HTML-CSS-and-JavaScript/tree/master/Lecture%202.%20Create%20and%20Style%20the%20Game%20Page)
-3. **Display Hard Coded Questions and Answers**
+3. [Display Hard Coded Questions and Answers](https://github.com/itsnewt/Build-A-Quiz-App-With-HTML-CSS-and-JavaScript/tree/master/Lecture%203.%20Display%20Hard%20Coded%20Questions%20and%20Answers)
 4. **Display Feedback for Correct/Incorrect Answers**
 5. **Create Head's Up Display (HUD)**
 6. **Create a Progress Bar**
@@ -16,9 +16,11 @@
 11. **Fetch API to Load Questions from Open Trivia DB API**
 12. **Create a Spinning Loader**
 
+<sup>I changed small bits of the code to fit my programming style more - such as using .querySelector instead of .getElementById</sup>
+
 ## Udemy Course Description
 
-Want to improve your core Web Develoment skills? Want to improve your knowledge of HTML, CSS, and JavaScript? In this course, you're going to learn how to build a Quiz application without the assistance of libraries or frameworks. Here are some of the topic we will cover!
+Want to improve your core Web Development skills? Want to improve your knowledge of HTML, CSS, and JavaScript? In this course, you're going to learn how to build a Quiz application without the assistance of libraries or frameworks. Here are some of the topic we will cover!
 
 - Save high scores in Local Storage
 - Create a progress bar
